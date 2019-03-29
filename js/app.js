058f8f2d6e50e7cb4bd5297b45c3fe0ae33fcc59
Chris Hamm <chris@bizstream.com>
2019-03-29 18:24:32 -0400

    initial search functionality

---
+++ js/app.js
@@ -0,0 +1,52 @@
+let apiKey = 'AIzaSyD3_QDaBntX4kwX5mgxTVvTihjg-zZSeOQ';
+
+
+
+
+$('.submit').on('click touch', function() {
+ let address = $('.address').val();
+ callApi(address);
+});
+
+ function callApi(address) {
+
+   fetch(`https://www.googleapis.com/civicinfo/v2/representatives?key=${apiKey}&address=${address}`).then(function(response) {
+     return response.json();
+    })
+    .then(function(results) {
+      console.log(results)
+      let offices = results.offices;
+      let officials = results.officials;
+
+
+
+      officials.forEach(e => {
+        let officialChannels = e.channels;
+        let channels = '';
+        let photoRow = '';
+
+        if(officialChannels != null){
+            officialChannels.forEach(e => {
+            channels += `<li><a href="http://www.${e.type}.com/${e.id}">${e.type}</a></li>`
+          });
+        }
+
+        if(e.photoUrl != null) {
+          photoRow = `<li><img src="${e.photoUrl}"/></li>`
+        }
+
+        $('.officials').append(
+          `<li>
+            ${e.name}
+            <ul>
+              ${photoRow}
+              <li>Party: ${e.party}</li>
+              <li>Phone: ${e.phone}</li>
+              <li><a href="${e.urls}">Website</a></li>
+              ${channels}
+            </ul>
+          </li>`
+        )
+      });
+    });
+  }
